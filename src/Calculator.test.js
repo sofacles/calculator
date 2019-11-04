b65d411f00b2d64400b83c43011bb7ecde51df15
steve <sofacles@yahoo.com>
2019-11-04 14:24:56 -0800

    failing subtraction test

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,15 +1,64 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import { getNodeText } from "@testing-library/dom"
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { getNodeText } from "@testing-library/dom";
 import Calculator from "./Calculator";
 
 describe("Calculator tests", () => {
+  afterEach(cleanup);
   it("updates display when you click 1", () => {
-    let { queryByText, getByTestId } = render(<Calculator />);;
+    let { queryByText, getByTestId } = render(<Calculator />);
     let oneKey = queryByText("1");
     fireEvent.click(oneKey);
-    
+
     const display = getByTestId("display");
     expect(getNodeText(display)).toEqual("1");
+  });
+  it("updates display when you click 1,0", () => {
+    let { queryByText, getByTestId } = render(<Calculator />);
+    let oneKey = queryByText("1");
+    fireEvent.click(oneKey);
+
+    let zeroKey = queryByText("0");
+    fireEvent.click(zeroKey);
+
+    const display = getByTestId("display");
+    expect(getNodeText(display)).toEqual("10");
+  });
+  describe("Addition: ", () => {
+    it("two plus two is four", () => {
+      const { queryByText, getByTestId } = render(<Calculator />);
+      let twoKey = queryByText("2");
+      fireEvent.click(twoKey);
+
+      let plusKey = queryByText("+");
+      fireEvent.click(plusKey);
+
+      fireEvent.click(twoKey);
+
+      let enterKey = queryByText("enter");
+      fireEvent.click(enterKey);
+
+      let display = getByTestId("display");
+      expect(getNodeText(display)).toEqual("4");
+    });
+  });
+
+  describe("Subtraction: ", () => {
+    it("8 minus two is 6", () => {
+      const { queryByText, getByTestId } = render(<Calculator />);
+      let eightKey = queryByText("2");
+      fireEvent.click(eightKey);
+
+      let minusKey = queryByText("-");
+      fireEvent.click(minusKey);
+
+      let twoKey = queryByText("2");
+
+      let enterKey = queryByText("enter");
+      fireEvent.click(enterKey);
+
+      let display = getByTestId("display");
+      expect(getNodeText(display)).toEqual("6");
+    });
   });
 });
