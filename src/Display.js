3a9c13a2fa564042add6b1838dc187637a6728a4
steve <sofacles@yahoo.com>
2019-11-03 17:38:04 -0800

    cleanup

--- src/Display.js
+++ src/Display.js
@@ -1,16 +1,16 @@
-import React, {useContext } from "react";
+import React, { useContext } from "react";
 import { calculatorContext } from "./CalculatorContext";
 
-
 const Display = () => {
-    let [state, setState] = useContext(calculatorContext);
-
-    const displayStyle = {
-        border: "1px solid #333",
-        height: "20px",
-        margin: "4px"
-    }
-    return <div style={displayStyle}>{state.stringCurrentlyBeingConcatenated}</div>
-}
+  let [state, setState] = useContext(calculatorContext);
+  const displayStyle = {
+    border: "1px solid #333",
+    height: "20px",
+    margin: "4px"
+  };
+  return (
+    <div style={displayStyle}>{state.stringCurrentlyBeingConcatenated}</div>
+  );
+};
 
 export default Display;
--- src/NumberKey.js
+++ src/NumberKey.js
@@ -1,17 +1,12 @@
 import React, { useContext } from "react";
-//import UseCalcReducer from "./useCalcReducer";
 import { calculatorContext } from "./CalculatorContext";
 import Key from "./Key";
 
 
 const NumberKey = (props) => {
-    //const {state, dispatch} = UseCalcReducer();
-    let [state, dispatch] = useContext(calculatorContext)
+   let [state, dispatch] = useContext(calculatorContext)
     const onPress = () => {
-        debugger;
         dispatch({ type: "DIGIT", payload: props.number});
-        // setState({...state,
-        //     stringCurrentlyBeingConcatenated: state.stringCurrentlyBeingConcatenated.concat(props.number)});
     };
 
     return <>
